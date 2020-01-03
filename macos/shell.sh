3bdf2c8d2fcb3ba82f9fd0c2e1edb0325ae301b9
nitro <nitrocode@users.noreply.github.com>
2020-01-03 15:31:27 -0500

    Update shell.sh

--- macos/shell.sh
+++ macos/shell.sh
@@ -3,3 +3,7 @@ sudo -s 'echo /usr/local/bin/zsh >> /etc/shells' && chsh -s /usr/local/bin/zsh
 
 # oh my zsh
 curl -L https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sh
+
+# zsh plugins that don't have a brew equivalent
+git clone https://github.com/djui/alias-tips.git $ZSH_CUSTOM/plugins/alias-tips
+git clone https://github.com/chrissicool/zsh-256color $ZSH_CUSTOM/plugins/zsh-256color
