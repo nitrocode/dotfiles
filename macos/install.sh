5b7b51f3c395e3012d98ef773c7c0cb798166a2a
nitro <nitrocode@users.noreply.github.com>
2020-01-07 00:33:50 -0500

    Update install.sh

--- macos/install.sh
+++ macos/install.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
+
+# first install xcode which is a dependency of gcc which is a dependency of xcrun which is a dependency of tfenv...
+xcode-select --install
+
 ./brew.sh
 ./configs.sh
 #./dock.sh
