1e772ab3e9e2c182fd4cffbc2f9d511cfa5e20ea
nitro <nitrocode@users.noreply.github.com>
2020-01-09 13:24:15 -0500

    Update brew.sh

--- macos/brew.sh
+++ macos/brew.sh
@@ -1,3 +1,8 @@
-# install brew
+# download Brewfile
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+# install from Brewfile
 brew bundle install
+# relink anything broken
+brew link
+# lastly, run the doctor to see anything out of whack
+brew doctor
