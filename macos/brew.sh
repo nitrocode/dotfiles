32c48ad0671200e53fe47b13ade778c6f3b8ff22
nitro <nitrocode@users.noreply.github.com>
2020-01-16 11:00:54 -0500

    brew.sh: add gnu softlinks

--- macos/brew.sh
+++ macos/brew.sh
@@ -6,3 +6,19 @@ brew bundle install
 brew link
 # lastly, run the doctor to see anything out of whack
 brew doctor
+
+# use the gnu tools instead of outdated mac tools
+# ls -la /usr/local/bin/g* | \
+#   grep coreutils | \
+#   cut -d'>' -f1 | \
+#   rev | \
+#   cut -d' ' -f2 | \
+#   rev | \
+#   while read gbin; do \
+#     ln -s "$gbin" "$(echo $gbin | sed 's,/usr/local/bin/g,/usr/local/bin/,g')"; \
+#   done
+ln -s /usr/local/bin/gmake /usr/local/bin/make
+## unnecessary?: ln -s /usr/local/bin/gawk /usr/local/bin/awk
+ln -s /usr/local/bin/gsed /usr/local/bin/sed
+ln -s /usr/local/bin/gtar /usr/local/bin/tar
+ln -s /usr/local/bin/ggrep /usr/local/bin/grep
