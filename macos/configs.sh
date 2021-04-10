1530963c0e1cd02bff399723bbc0dba132b72684
nitro <nitrocode@users.noreply.github.com>
2021-04-09 20:53:09 -0400

    Update configs.sh

--- macos/configs.sh
+++ macos/configs.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
+# more located at https://github.com/herrbischoff/awesome-macos-command-line
+
 # show osx hidden files by default
 defaults write com.apple.finder AppleShowAllFiles NO
 
+# Avoids creation of .DS_Store and AppleDouble files.
+defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool true
