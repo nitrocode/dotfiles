d703f257815dd166ecf08086f480f4dd1a392da2
nitrocode <nitrocode@users.noreply.github.com>
2021-08-22 11:42:45 -0400

    agnostic: install vscode and docker

--- agnostic/vagrant-bootstrap.sh
+++ agnostic/vagrant-bootstrap.sh
@@ -3,9 +3,13 @@ sudo apt install apt-transport-https -y
 
 # Install login manager and desktop environment
 # https://cialu.net/how-to-solve-failed-to-start-session-with-lightdm-and-xfce/
-sudo apt install xfce4 lightdm -y
+sudo apt install -y \
+  xfce4 \
+  lightdm
+
 cp /usr/share/lightdm/lightdm.conf.d/50-unity-greeter.conf \
-   /usr/share/lightdm/lightdm.conf.d/50-unity-greeter.conf.bak
+  /usr/share/lightdm/lightdm.conf.d/50-unity-greeter.conf.bak
+
 cat <<EOF > /usr/share/lightdm/lightdm.conf.d/50-unity-greeter.conf
 [SeatDefaults]
 greeter-session=unity-greeter
@@ -18,5 +22,25 @@ wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > pa
 sudo install -o root -g root -m 644 packages.microsoft.gpg /etc/apt/trusted.gpg.d/
 sudo sh -c 'echo "deb [arch=amd64 signed-by=/etc/apt/trusted.gpg.d/packages.microsoft.gpg] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
 
+# install docker
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository \
+  "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) \
+  stable"
+
+# install all the packages
+
 sudo apt update
-sudo apt install code -y
+sudo apt install -y \
+  code \
+  terminator \
+  apt-transport-https \
+  ca-certificates \
+  curl \
+  gnupg-agent \
+  software-properties-common \
+  docker-ce \
+  docker-ce-cli
+
+sudo usermod -aG docker $(whoami)
