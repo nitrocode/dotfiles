38be34f9a8e57c0ad309023b03af9af0540ad5f3
nitrocode <nitrocode@users.noreply.github.com>
2021-08-22 20:08:26 -0400

    Auto login, xfce defaults, docker group

--- agnostic/vagrant-bootstrap.sh
+++ agnostic/vagrant-bootstrap.sh
@@ -1,36 +1,50 @@
+#!/bin/sh
 sudo apt update
 sudo apt install apt-transport-https -y
 
 # Install login manager and desktop environment
-# https://cialu.net/how-to-solve-failed-to-start-session-with-lightdm-and-xfce/
+# source: https://cialu.net/how-to-solve-failed-to-start-session-with-lightdm-and-xfce/
 sudo apt install -y \
   xfce4 \
   lightdm
 
-cp /usr/share/lightdm/lightdm.conf.d/50-unity-greeter.conf \
-  /usr/share/lightdm/lightdm.conf.d/50-unity-greeter.conf.bak
+echo "xfce4-session" > .xsession
 
+# Update 50-unity-greeter to add user-session
+cp /usr/share/lightdm/lightdm.conf.d/50-unity-greeter.conf{,.bak}
 cat <<EOF > /usr/share/lightdm/lightdm.conf.d/50-unity-greeter.conf
 [SeatDefaults]
 greeter-session=unity-greeter
 user-session=xfce
 EOF
 
-# install vscode
-# https://code.visualstudio.com/docs/setup/linux
+# Login to $DESKTOP_SESSION (which returns xfce)
+cat <<EOF > /etc/lightdm/lightdm.conf.d/0-autologin.conf
+[Seat:*]
+autologin-session=xfce
+autologin-user=vagrant
+autologin-user-timeout=1
+EOF
+
+# Ignore "Use default" prompt when first booting into xfce
+# source: https://mail.xfce.org/pipermail/xfce/2015-October/034685.html
+cp /etc/xdg/xfce4/panel/default.xml /etc/xdg/xfce4/xfconf/xfce-perchannel-xml/xfce4-panel.xml
+
+# Install vscode
+# source: https://code.visualstudio.com/docs/setup/linux
 wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
 sudo install -o root -g root -m 644 packages.microsoft.gpg /etc/apt/trusted.gpg.d/
+rm packages.microsoft.gpg
 sudo sh -c 'echo "deb [arch=amd64 signed-by=/etc/apt/trusted.gpg.d/packages.microsoft.gpg] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
 
-# install docker
+# Install docker
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository \
   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
   $(lsb_release -cs) \
   stable"
 
-# install all the packages
-
+# Install some of the base packages I like
 sudo apt update
 sudo apt install -y \
   code \
@@ -43,4 +57,16 @@ sudo apt install -y \
   docker-ce \
   docker-ce-cli
 
-sudo usermod -aG docker $(whoami)
+# Setup the docker group
+## add the group
+sudo groupadd docker
+sudo newgrp docker
+## associate vagrant user with the docker group
+sudo usermod -aG docker vagrant
+sudo gpasswd -a vagrant docker
+## enable docker
+sudo systemctl enable docker
+sudo systemctl start docker
+
+# may need a reboot
+reboot
