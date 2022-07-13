65903ed9be409ba2052a3a9bf641b7fe1904cd88
nitrocode <7775707+nitrocode@users.noreply.github.com>
2022-07-13 12:32:28 -0500

    Use which instead of hard coding paths

--- macos/shell.sh
+++ macos/shell.sh
@@ -1,6 +1,6 @@
 # default shell
-echo "/usr/local/bin/zsh" | sudo tee -a /etc/shells
-chsh -s /usr/local/bin/zsh
+echo $(which zsh) | sudo tee -a /etc/shells
+chsh -s $(which zsh)
 
 # oh my zsh
 curl -L https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sh
